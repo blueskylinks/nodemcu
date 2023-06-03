3fcbc80bca9165d8b03a414a93d9ef2b2949d7ff
Chaitanya <chaitanya.csd@gmail.com>
2023-06-03 14:02:55 +0530

    water level v3 - 02 06

--- water_level_v3/water_level_v3.ino
+++ water_level_v3/water_level_v3.ino
@@ -17,12 +17,13 @@ void setup() {
   Serial.begin(115200); // Starts the serial communication
   tank_size=40;
   len=tank_size/10;
+  pinMode(D3, OUTPUT); 
   pinMode(D4, OUTPUT);
   pinMode(D5, OUTPUT);  //GPIO14
   pinMode(D6, OUTPUT);  //GPIO12
   pinMode(D7, OUTPUT);  //GPIO13
   pinMode(D8, OUTPUT);
-  pinMode(D3, INPUT_PULLUP); 
+  
   tank_blinkall();
   // Buzzer LED 
  
@@ -49,13 +50,13 @@ void loop() {
   // Convert to inches
   distanceInch = distanceCm * CM_TO_INCH;
   //=====================================================================================
-  
+
   // Prints the distance on the Serial Monitor
   Serial.print("Distance (cm):\n ");
   Serial.println(distanceCm);
   Serial.print("Distance (inch): ");
   Serial.println(distanceInch);
-  cap=distanceInch/(len*1);
+  cap=distanceInch/(len*1.4);
   Serial.print("Capacity:");
   cap1=(((int)cap)+0);
   Serial.println(cap1);
@@ -85,8 +86,9 @@ void loop() {
     break;
 
     case 2:
-      digitalWrite(D4,LOW);
-      digitalWrite(D5,HIGH);
+      digitalWrite(D3,LOW);
+      digitalWrite(D4,HIGH);
+      digitalWrite(D5,LOW);
       digitalWrite(D6,LOW);
       digitalWrite(D7,LOW);
       digitalWrite(D8,LOW); 
@@ -94,46 +96,60 @@ void loop() {
     break;
 
     case 3: 
+      digitalWrite(D3,LOW);
       digitalWrite(D4,LOW);
-      digitalWrite(D5,LOW);
-      digitalWrite(D6,HIGH);
+      digitalWrite(D5,HIGH);
+      digitalWrite(D6,LOW);
       digitalWrite(D7,LOW);
       digitalWrite(D8,LOW); 
       Serial.println("Level 3");
     break;
 
     case 4:
+      digitalWrite(D3,LOW);
       digitalWrite(D4,LOW);
       digitalWrite(D5,LOW);
-      digitalWrite(D6,LOW);
-      digitalWrite(D7,HIGH);
+      digitalWrite(D6,HIGH);
+      digitalWrite(D7,LOW);
       digitalWrite(D8,LOW); 
       Serial.println("Level 4");
     break;
 
     case 5:
+      digitalWrite(D3,LOW);
+      digitalWrite(D4,LOW);
+      digitalWrite(D5,LOW);
+      digitalWrite(D6,LOW);
+      digitalWrite(D7,HIGH);
+      digitalWrite(D8,LOW); 
+      Serial.println("Level 5");
+    break;
+
+    case 6:
+      digitalWrite(D3,LOW);
       digitalWrite(D4,LOW);
       digitalWrite(D5,LOW);
       digitalWrite(D6,LOW);
       digitalWrite(D7,LOW);
       digitalWrite(D8,HIGH); 
-      Serial.println("Level 5");
+      Serial.println("Level 6");
       tank_empty();
-    break;
       
     default:
+      digitalWrite(D3,LOW);
       digitalWrite(D4,LOW);
       digitalWrite(D5,LOW);
       digitalWrite(D6,LOW);
       digitalWrite(D7,LOW);
-      digitalWrite(D8,LOW);                                
+      digitalWrite(D8,LOW); 
+      Serial.println("Level Default");                                
    }
     delay(1000);
     count=count+1;
     Serial.println(count);
     if(count>=60){
       Serial.println("Sleep Mode Start.....");
-      ESP.deepSleep(1800e6);
+      ESP.deepSleep(3600e6);
     }
  }
 
@@ -161,18 +177,20 @@ void loop() {
  }
 
   void tank_blinkall(){
-     
+     digitalWrite(D3,HIGH);
+     delay(50);
      digitalWrite(D4,HIGH);
-     delay(100);
+     delay(50);
      digitalWrite(D5,HIGH);
-     delay(100);
+     delay(50);
      digitalWrite(D6,HIGH);
-     delay(100);
+     delay(50);
      digitalWrite(D7,HIGH);
-     delay(100);
+     delay(50);
      digitalWrite(D8,HIGH);     
-     delay(100);
+     delay(50);
 
+     digitalWrite(D3,LOW);
      digitalWrite(D4,LOW);
      digitalWrite(D5,LOW);
      digitalWrite(D6,LOW);
